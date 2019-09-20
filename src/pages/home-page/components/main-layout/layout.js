a7fadd554c737b2a2be672d51e610e9d85dac371
babbage <601728820@qq.com>
2019-09-20 10:10:44 +0800

    基本布局已生成，main-layout分成left-nav,header,main-frame,footer,并新增了行检测 现在通过main-layout的参数传递对应组件可以控制主布局了

--- src/pages/home-page/components/main-layout/layout.js
+++ src/pages/home-page/components/main-layout/layout.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import { Layout, Icon, Card } from "antd";
-
+import PropTypes from "prop-types";
 import "./layout.css";
 
 const { Header, Content, Footer, Sider } = Layout;
 
-export default function MainLayout() {
+export default function MainLayout(props) {
   const [collapsed, setCollapsed] = useState(false);
+
   return (
     <div className="mainLayout">
       <Layout>
@@ -24,7 +25,8 @@ export default function MainLayout() {
               color: "#fff"
             }}
           >
-            leftNav
+            {/* 这里需要传递左侧菜单 leftNav 组件 */}
+            {props.leftNav}
           </span>
         </Sider>
         <Layout className={`main-frame ${collapsed && "left-nav-collapsed"}`}>
@@ -34,14 +36,29 @@ export default function MainLayout() {
               type={collapsed ? "menu-unfold" : "menu-fold"}
               onClick={event => setCollapsed(!collapsed)}
             />
-            Header
+            {/* 这里需要传递header 组件 */}
+            {props.header}
           </Header>
           <Content className="main-content">
-            <Card style={{ height: "100%" }}>Content Area</Card>
+            <Card style={{ height: "100%" }}>
+              {/* 这里需要传递mainFrame 组件，填充右侧内容区域的组件 */}
+              {props.mainFrame}
+            </Card>
           </Content>
-          <Footer style={{ textAlign: "center" }}>这里是Footer</Footer>
+          <Footer style={{ textAlign: "center" }}>
+            {/* 这里需要传递footer 组件,可选 */}
+            {props.footer}
+          </Footer>
         </Layout>
       </Layout>
     </div>
   );
 }
+
+// 需要的参数说明
+MainLayout.propTypes = {
+  leftNav: PropTypes.node.isRequired,
+  header: PropTypes.node.isRequired,
+  mainFrame: PropTypes.node.isRequired,
+  footer: PropTypes.node
+};
--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -3,5 +3,12 @@ import React from "react";
 import MainLayout from "./components/main-layout/";
 
 export default function HomePage() {
-  return <MainLayout></MainLayout>;
+  return (
+    <MainLayout
+      leftNav="leftNav"
+      header="Header"
+      mainFrame="mainFrame"
+      footer="Footer"
+    />
+  );
 }
