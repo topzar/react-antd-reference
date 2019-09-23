e253681eae649a987258a36453de34bf45718c7e
babbage <601728820@qq.com>
2019-09-23 23:07:07 +0800

    demo 页面删除,新增了菜单组件实例程序

--- src/pages/home-page/components/left-nav/index.js
+++ src/pages/home-page/components/left-nav/index.js
@@ -1,31 +1,18 @@
 import React from "react";
 import { Menu, Icon } from "antd";
-
 import { Link } from "react-router-dom";
 
 import "./index.less";
+
 function LeftNav() {
   return (
     <div>
+      <div className="logo" />
       <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
         <Menu.Item key="1">
-          <Icon type="user" />
+          <Icon type="menu" />
           <span>
-            <Link to="/demo1">demo 1</Link>
-          </span>
-        </Menu.Item>
-        <Menu.Item key="2">
-          <Icon type="video-camera" />
-
-          <span>
-            <Link to="/demo2">demo 2</Link>
-          </span>
-        </Menu.Item>
-        <Menu.Item key="3">
-          <Icon type="upload" />
-
-          <span>
-            <Link to="/demo3">demo 3</Link>
+            <Link to="/menu-demo">菜单展示页</Link>
           </span>
         </Menu.Item>
       </Menu>
--- src/routers.js
+++ src/routers.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import { Card } from "antd";
 
 import HomePage from "./pages/home-page";
 
-import Demo1 from "./pages/demo-1/";
-import Demo2 from "./pages/demo-2/";
-import Demo3 from "./pages/demo-3/";
+import MenuDemo from "./pages/menu-demo";
 
 export default function Routers() {
   return (
@@ -18,11 +16,7 @@ export default function Routers() {
           return (
             <HomePage>
               <Card style={{ height: "100%" }}>
-                <Switch>
-                  <Route path="/demo1" component={Demo1} />
-                  <Route path="/demo2" component={Demo2} />
-                  <Route path="/demo3" component={Demo3} />
-                </Switch>
+                <Route path="/menu-demo" component={MenuDemo} />
               </Card>
             </HomePage>
           );
