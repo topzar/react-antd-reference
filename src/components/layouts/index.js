3a69c0fc241523fb587c3a004d100b5d99fdc6f5
babbage <601728820@qq.com>
2019-09-25 16:25:04 +0800

    类名重整,布局微调

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -6,15 +6,15 @@ import "./index.less";
 const { Header, Content, Footer, Sider } = Layout;
 export default function BaseLayout() {
   return (
-    <Layout className="base-layout">
-      <Sider className="leftNav">
+    <Layout className="full-container">
+      <Sider className="sider">
         <div className="logo" />
         leftNav
       </Sider>
-      <Layout className="main-layout">
+      <Layout className="main-frame">
         <Header className="header">Header</Header>
-        <Content className="container">
-          <div className="content">Main content area</div>
+        <Content className="content">
+          <div className="content-container">Main content area</div>
         </Content>
         <Footer className="footer">Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
