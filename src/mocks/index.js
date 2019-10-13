145ac98bb9cd804a1873695646fc83d5c7cf519a
babbage <601728820@qq.com>
2019-10-13 13:33:32 +0800

    feat: mockjs扩展功能,新增logger
    原来的mockjs没有日志功能,对调试不友好,现在扩展了日志功能

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -2,15 +2,46 @@ import Mock from "mockjs";
 import { userLogin } from "@mocks/user";
 import { projectList } from "@mocks/project";
 
-// 配置Ajax请求延时，可用来测试网络延迟大时项目中一些效果
-Mock.setup({
+class CustomizedMock {
+  constructor(setupConfigs = {}) {
+    // 配置Ajax请求延时，可用来测试网络延迟大时项目中一些效果
+    Mock.setup(setupConfigs);
+    this.mockList = {};
+
+    this.logger = req => this.mockLogger(req);
+  }
+
+  genRequestKey(url, requestType) {
+    return window.btoa(requestType + "#" + url);
+  }
+
+  mockLogger(req) {
+    console.log("mockLogger -> req", req);
+    let newKey = this.genRequestKey(
+      req.url.replace(new RegExp("http://localhost"), ""),
+      req.type
+    );
+    let resp = this.mockList[newKey](req);
+    console.log("mockLogger -> resp", resp);
+
+    return resp;
+  }
+
+  mock(url, requestType, template) {
+    let key = this.genRequestKey(url, requestType.toLocaleUpperCase());
+    this.mockList[key] = template;
+    Mock.mock(new RegExp(url), requestType, this.logger);
+  }
+}
+
+const customMock = new CustomizedMock({
   timeout: 1000
 });
 
 // 用户
-Mock.mock(new RegExp("/api/user/login"), "post", userLogin);
-
+// Mock.mock(new RegExp("/api/user/login"), "post", userLogin);
+customMock.mock("/api/user/login", "post", userLogin);
 // 项目
-Mock.mock(new RegExp("/api/project/list"), "post", projectList);
+customMock.mock("/api/project/list", "post", projectList);
 
 export default Mock;
